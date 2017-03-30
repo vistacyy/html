7b23bda745eb431ba185b61eb721761e96d94c33
vistacyy <vistacyy@163.com>
2017-03-30 23:16:03 +0800

    'indexedDB'

--- index.js
+++ index.js
@@ -1,73 +1,69 @@
-
-let datatable = document.getElementById('datatable');
-// 创建数据库
-let db = openDatabase('MyData', '', 'My database', 102400);
-// 显示所有数据
-function init() {
-  showAllData();
-}
-// 隐藏全部数据
-function removeAllData() {
-  for (let i = datatable.childNodes.length - 1; i > 0; i--) {
-    datatable.removeChild(datatable.childNodes[i]);
-  }
-}
-// 创建表头
-let tr = document.createElement('tr');
-let th1 = document.createElement('th');
-let th2 = document.createElement('th');
-let th3 = document.createElement('th');
-th1.innerHTML = '姓名';
-th2.innerHTML = '留言';
-th3.innerHTML = '时间';
-tr.appendChild(th1);
-tr.appendChild(th2);
-tr.appendChild(th3);
-datatable.appendChild(tr);
-
-// 显示出所有数据
-function showData(row) {
-  let tr = document.createElement('tr');
-  let td1 = document.createElement('td');
-  td1.innerHTML = row.name;
-  let td2 = document.createElement('td');
-  td2.innerHTML = row.message;
-  let td3 = document.createElement('td');
-  let t = new Date();
-  t.setTime(row.time);
-  td3.innerHTML = t.toLocaleDateString() + ' ' + t.toLocaleTimeString();
-  tr.appendChild(td1);
-  tr.appendChild(td2);
-  tr.appendChild(td3);
-  datatable.appendChild(tr);
-}
-
-function showAllData() {
-  db.transaction(function (tx) {
-    tx.executeSql('create table if not exists msgdata(name text,message text,time interge)', []);
-    tx.executeSql('select * from msgdata', [], function (tx, rs) {
-      removeAllData();
-      for (let i = 0; i < rs.rows.length; i++) {
-        showData(rs.rows[i]);
-      }
-    });
-  });
-}
-// 添加数据
-function addData(name, message, time) {
-  db.transaction(function (tx) {
-    tx.executeSql('insert into msgdata values(?,?,?)', [name, message, time], function (tx, rs) {
+window.indexedDB=window.indexedDB || window.webkitIndexedDB || window.mozIndexedDB || window.msIndexedDB;
+window.IDBTransaction=window.IDBTransaction || window.webkitIDBTransaction || window.msIDBTransaction;
+window.IDBKeyRange=window.IDBKeyRange || window.webkitIDBKeyRange || window.msIDBKeyRange;
+window.IDBCursor=window.IDBCursor || window.webkitIDBCursor || window.msIDBCursor;
+function connectDatabase(){
+  let dbName='indexedDbTest';
+  let dbVersion=20150307;
+  let idb;
+  let dbConnect=indexedDB.open(dbName,dbVersion);
+  dbConnect.onsuccess=function(e){
+    idb=e.target.result;
+    console.log('连接成功',e);
+    // 开启事务
+    let tx=idb.transaction(['Users'],'readwrite');
+    let store=tx.objectStore('Users');
+    let value={
+      userId:1,
+      userName:'张三',
+      address:'住址1'
+    };
+    let req=store.put(value);
+    req.onsuccess=function(e){
       alert('数据保存成功');
-    }, function (tx, error) {
-      alert(error.source + '::' + error.message);
-    });
-  });
-}
+    };
+    req.onerror=function(e){
+      alert('数据保存失败');
+    };
+    let reqGut=store.get(1);
+    reqGut.onsuccess=function(e){
+      if(this.result == undefined){
+        alert('无数据');
+      }else{
+        console.log('获取数据成功',this.result);
+      }
+    };
+    reqGut.onerror=function(){
+      alert('获取数据失败');
+    };
+  };
+  dbConnect.onerror=function(e){
+    alert('连接失败');
+  };
+  
+  // 版本更新事务
+  dbConnect.onupgradeneeded=function(e){
+    idb=e.target.result;
+    let tx=e.target.transaction;
+    // let oldVersion=e.oldVersion;
+    // let newVersion=e.newVersion;
+    // alert('数据库版本更新成功，旧版本号为'+oldVersion+',新版本号为'+newVersion);
+    let name='Users';
+    let optionalParameters={
+      keyPath:'userId',
+      autoIncreament:false
+    };
+    let store=idb.createObjectStore(name,optionalParameters);
+    alert('对象仓库创建成功');
+    let indexName='userNameIndex';
+    let keyPath='userName';
+    let indexParameters={
+      unique:false,
+      multiEntry:false
+    };
+    let idx=store.createIndex(name,keyPath,indexParameters);
+    alert('创建索引成功');
+  };
 
-function saveData() {
-  let name = document.getElementById('name').value;
-  let memo = document.getElementById('memo').value;
-  let time = new Date().getTime();
-  addData(name, memo, time);
-  showAllData();
+
 }
