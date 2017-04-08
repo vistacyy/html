12666ee06b70bf85ec666759f066ae6ae7ca1619
vistacyy <vistacyy@163.com>
2017-04-08 11:41:29 +0800

    rtc

---
+++ serverSent.php
@@ -0,0 +1,8 @@
+<?php
+  header('Content-Type:text/event-stream;charset:utf-8');
+  header('Cache-Control:no-cache');
+  date_default_timezone_set('Asia/Shanghai'); 
+  echo "event:test\n\n";
+  echo 'data:服务器时间:'.date('Y/m/d H:i:s')."\n\n";
+  flush();
+?>
